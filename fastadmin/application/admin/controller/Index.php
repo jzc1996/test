5524b29429a5edbe92826d9122583abf6c37acc5
jzc1996 <1937359095@qq.com>
2021-11-17 14:15:38 +0800

    add

--- fastadmin/application/admin/controller/Index.php
+++ fastadmin/application/admin/controller/Index.php
@@ -186,7 +186,7 @@ class Index //extends Backend
 
     public function ceshiRedis()
     {
-        $redis = new Redis();
+        $redis = new \Redis();
         $redis->connect('127.0.0.1', 6379);
         echo "Connection to server successfully";
         //查看服务是否运行
